ca2f2fdea6ff755b17e163e383e5317c8d878322
Anar-Qandayev <99839527+Anar-Qandayev@users.noreply.github.com>
2022-05-31 12:16:09 +0400

    Update Category.cs

--- Nest2/Models/Category.cs
+++ Nest2/Models/Category.cs
@@ -10,5 +10,7 @@ namespace Nest2.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Logo { get; set; }
+        public bool IsDeleted { get; set; }
+        public List<Product> Products { get; set; }
     }
 }
